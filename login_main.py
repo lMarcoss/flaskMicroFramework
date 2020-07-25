a70119cd06c8ec0932f2915278ce0147f717c237
lmarcoss <leonardo.marcos@segurossura.com.mx>
2020-07-25 15:29:21 -0500

    cookies

--- login_main.py
+++ login_main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import make_response
 from flask_wtf import CSRFProtect
 
 import login
@@ -12,7 +13,10 @@ csrf = CSRFProtect(app)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    # if no found custome_cookie return value_undefined
+    custome_cookie = request.cookies.get('custome_cookie', 'value_undefined')
+    print(custome_cookie)
+    return render_template('index.html')
 
 
 @app.route('/login/', methods=['GET'])
@@ -27,6 +31,14 @@ def authentication():
     print(login_form.username.data)
     print(login_form.password.data)
     return render_template('login.html', title='login', form=login_form)
+
+
+@app.route('/cookie/')
+def cookie():
+    # las cookies trabajan sobre el objeto response
+    response = make_response(render_template('cookie.html'))
+    response.set_cookie('custome_cookie', 'Marcos Santiago Leonardo')
+    return response
 
 
 # Levanta  el servidor app.run() default port, and mode debug false
