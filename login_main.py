027ed2cd7bcfeada36168cb61bd2c7d16c787d29
lmarcoss <leonardo.marcos@segurossura.com.mx>
2020-07-25 15:45:42 -0500

    session

--- login_main.py
+++ login_main.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask import make_response
 from flask import render_template
 from flask import request
-from flask import make_response
+from flask import session
+from flask import redirect
+from flask import url_for
 from flask_wtf import CSRFProtect
 
-import login
+import user_login
 
 app = Flask(__name__, template_folder='files_html')
 app.secret_key = 'my_secret_key'
@@ -15,21 +18,27 @@ csrf = CSRFProtect(app)
 def index():
     # if no found custome_cookie return value_undefined
     custome_cookie = request.cookies.get('custome_cookie', 'value_undefined')
-    print(custome_cookie)
+    print('custome_cookie = ', custome_cookie)
+    if 'username' in session:
+        username = session['username']
+        print('username ', username)
     return render_template('index.html')
 
 
 @app.route('/login/', methods=['GET'])
-def show_form_login():
-    login_form = login.LoginForm()
+def f_login():
+    login_form = user_login.LoginForm()
     return render_template('login.html', title='login', form=login_form)
 
 
 @app.route('/authentication/', methods=['POST'])
 def authentication():
-    login_form = login.LoginForm(request.form)
-    print(login_form.username.data)
-    print(login_form.password.data)
+    login_form = user_login.LoginForm(request.form)
+    if login_form.validate():
+        session['username'] = login_form.username.data
+        print(login_form.username.data)
+        print(login_form.password.data)
+
     return render_template('login.html', title='login', form=login_form)
 
 
@@ -39,6 +48,13 @@ def cookie():
     response = make_response(render_template('cookie.html'))
     response.set_cookie('custome_cookie', 'Marcos Santiago Leonardo')
     return response
+
+
+@app.route('/logout/')
+def logout():
+    if 'username' in session:
+        session.pop('username')
+    return redirect(url_for('f_login'))
 
 
 # Levanta  el servidor app.run() default port, and mode debug false
